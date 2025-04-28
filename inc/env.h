8d1f23c6bdea83694cec67eafa704dcdc5b68240
Antoine Bouclier <abouclie@z4r12p5.42lyon.fr>
2025-04-28 11:57:28 +0200

    env.h norminette OK

--- inc/env.h
+++ inc/env.h
@@ -6,24 +6,24 @@
 /*   By: abouclie <abouclie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 11:13:44 by abouclie          #+#    #+#             */
-/*   Updated: 2025/04/28 11:51:48 by abouclie         ###   ########.fr       */
+/*   Updated: 2025/04/28 11:56:32 by abouclie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef	READLINE_H
-# define READLINE_H
+#ifndef ENV_H
+# define ENV_H
 
-#include <stdlib.h>
-#include <limits.h>
-#include <unistd.h>
-#include <stdio.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <unistd.h>
+# include <stdio.h>
 
-typedef struct	s_env
+typedef struct s_env
 {
-	char *key;			// ex: "PATH"
-	char *value;		// ex: "/usr/bin:/bin"
-	struct s_env *next;
-} t_env;
+	char			*key;
+	char			*value;
+	struct s_env	*next;
+}				t_env;
 
 /* Prototypes */
 int		ft_echo(char **args);
@@ -32,7 +32,7 @@ void	malloc_fail(void);
 void	free_split(char **split);
 
 /* ft_export */
-int	ft_export(char **args, t_env **env);
+int		ft_export(char **args, t_env **env);
 
 void	print_sorted_env(t_env *env);
 
@@ -41,6 +41,6 @@ t_env	*copy_env_list(t_env *env);
 t_env	*search_env_key(t_env *env, const char *key);
 
 /* Utils */
-int count_args(char **args);
+int		count_args(char **args);
 
 #endif
