1e2f92e28f2e7c04bedc6209bd445503220ac334
Antoine Bouclier <abouclie@z4r12p5.42lyon.fr>
2025-04-29 09:50:49 +0200

    env update (j'avais oublié de mettre a jour la fonction)

--- src/builtins/ft_env.c
+++ src/builtins/ft_env.c
@@ -6,14 +6,30 @@
 /*   By: abouclie <abouclie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 10:15:48 by abouclie          #+#    #+#             */
-/*   Updated: 2025/04/23 10:16:16 by abouclie         ###   ########.fr       */
+/*   Updated: 2025/04/29 09:47:43 by abouclie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "env.h"
 
-int	ft_env(t_env *env)
+int	ft_env(t_env *env, int argc, char **args)
 {
+	char	*perror_msg;
+	int		i;
+
+	i = 1;
+	if (argc >= 2)
+	{
+		while (args[i])
+		{
+			if (args[i][0] == '-')
+				ft_printf("env: '%s': no option allowed\n", args[i]);
+			else
+				ft_printf("env: '%s': no argument allowed\n", args[i]);
+			i++;
+		}
+		return (1);
+	}
 	while (env)
 	{
 		if (env->value)
