8aa08b9a9c385060d05444f598f3ab3dad8fe6bd
Antoine Bouclier <abouclie@z4r12p5.42lyon.fr>
2025-04-29 14:17:58 +0200

    ft_exit - norme OK

--- src/builtins/ft_exit.c
+++ src/builtins/ft_exit.c
@@ -6,13 +6,51 @@
 /*   By: abouclie <abouclie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 10:13:26 by abouclie          #+#    #+#             */
-/*   Updated: 2025/04/23 10:16:42 by abouclie         ###   ########.fr       */
+/*   Updated: 2025/04/29 14:15:48 by abouclie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "env.h"
 
-int	ft_exit(args)
+static int	str_is_numeric(char *arg)
 {
-	
+	int	i;
+
+	i = 0;
+	if (arg[i] == '-' || arg[i] == '+')
+	{
+		i++;
+	}
+	while (arg[i])
+	{
+		if (ft_isdigit(arg[i]) == 0)
+			return (2);
+		i++;
+	}
+	return (0);
+}
+
+int	ft_exit(int argc, char **args)
+{
+	int	arg;
+
+	if (argc > 2)
+	{
+		ft_printf("exit: too many arguments\n", args[1]);
+		return (1);
+	}
+	if (argc == 2)
+	{
+		if (str_is_numeric(args[1]) == 2)
+		{
+			ft_printf("exit: %s: numeric argument required\n", args[1]);
+			exit(2);
+		}
+		else
+		{
+			arg = ft_atoi(args[1]) % 256;
+			exit(arg);
+		}
+	}
+	exit(0);
 }
