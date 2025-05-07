6f8530df44fd99b7e236cd1cef0d6ce56032d7c2
Antoine Bouclier <abouclie@z4r12p5.42lyon.fr>
2025-05-07 12:11:59 +0200

    norme echo

--- src/builtins/ft_echo.c
+++ src/builtins/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: abouclie <abouclie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 11:07:55 by abouclie          #+#    #+#             */
-/*   Updated: 2025/05/06 12:46:39 by abouclie         ###   ########.fr       */
+/*   Updated: 2025/05/07 12:09:39 by abouclie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,23 +28,17 @@ bool	is_dash_n_flag(char *arg)
 	return (true);
 }
 
-int	ft_echo(char **args)
+void	check_n_flags(char **args, int *i, bool *newline)
 {
-	int		i;
-	bool	newline;
+	while (args[*i] && is_dash_n_flag(args[*i]))
+	{
+		*newline = false;
+		(*i)++;
+	}
+}
 
-	i = 1;
-	newline = true;
-	while (args[i] && is_dash_n_flag(args[i]))
-	{
-		newline = false;
-		i++;
-	}
-	if (!args[1])
-	{
-		ft_printf("\n");
-		return (EXIT_SUCCESS);
-	}
+void	print_echo_args(char **args, int i)
+{
 	while (args[i])
 	{
 		ft_printf("%s", args[i]);
@@ -52,6 +46,23 @@ int	ft_echo(char **args)
 			ft_printf(" ");
 		i++;
 	}
+}
+
+int	ft_echo(char **args)
+{
+	int		i;
+	bool	newline;
+
+	i = 1;
+	newline = true;
+	if (!args[1])
+	{
+		ft_printf("\n");
+		free(args);
+		return (EXIT_SUCCESS);
+	}
+	check_n_flags(args, &i, &newline);
+	print_echo_args(args, i);
 	if (newline)
 		ft_printf("\n");
 	free(args);
