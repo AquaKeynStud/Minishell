1f2f7c31140ab585ac184454e1b21e96ae900cfd
Antoine Bouclier <abouclie@z4r12p5.42lyon.fr>
2025-05-14 14:38:18 +0200

    leaks exit OK

--- src/builtins/ft_exit.c
+++ src/builtins/ft_exit.c
@@ -6,18 +6,21 @@
 /*   By: abouclie <abouclie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 10:13:26 by abouclie          #+#    #+#             */
-/*   Updated: 2025/05/14 12:49:02 by abouclie         ###   ########.fr       */
+/*   Updated: 2025/05/14 14:35:41 by abouclie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "env.h"
 #include "minishell.h"
+#include "parsing.h"
 
 static int	str_is_numeric(char *arg)
 {
 	int	i;
 
 	i = 0;
+	if (!arg[0])
+		return (2);
 	if (arg[i] == '-' || arg[i] == '+')
 		i++;
 	while (arg[i])
@@ -27,6 +30,16 @@ static int	str_is_numeric(char *arg)
 		i++;
 	}
 	return (0);
+}
+
+static void	free_exit(t_ctx *ctx, char **args)
+{
+	free(args);
+	if (ctx->ast)
+		free_ast(ctx->ast);
+	if (ctx->tokens)
+		free_tokens(&ctx->tokens);
+	free(ctx->input);
 }
 
 static int	check_exit_args(int argc, char **args)
@@ -51,34 +64,36 @@ static int	check_exit_args(int argc, char **args)
 void	handle_exit(t_ctx *ctx, int argc, char **args)
 {
 	long long	arg;
-	int			error;
 
 	if (argc > 1)
 	{
-		arg = ft_atoll(args[1], &error) % 256;
-		if (error == 2)
+		arg = ft_atoll(args[1], &ctx->status) % 256;
+		if (ctx->status == 2)
 		{
 			ft_dprintf(2, "minishell: exit: ");
 			ft_dprintf(2, "%s: numeric argument required\n", args[1]);
-			ctx->status = error;
+			ctx->status = ctx->status;
+			free_exit(ctx, args);
 			secure_exit(ctx);
 		}
 		ctx->status = arg % 256;
+		free_exit(ctx, args);
 		secure_exit(ctx);
 	}
-	free(args);
+	free_exit(ctx, args);
 	secure_exit(ctx);
 }
 
 int	ft_exit(t_ctx *ctx, int argc, char **args)
 {
-	int	error;
-
-	error = check_exit_args(argc, args);
-	if (error)
+	ctx->status = check_exit_args(argc, args);
+	if (ctx->status)
 	{
-		if (error == 2)
-			exit(2);
+		if (ctx->status == 2)
+		{
+			free_exit(ctx, args);
+			secure_exit(ctx);
+		}
 		return (1);
 	}
 	handle_exit(ctx, argc, args);
