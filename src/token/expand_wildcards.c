f22ec3408d57efec9e35b98391c33a3e4f45619c
Antoine Bouclier <abouclie@z4r10p4.42lyon.fr>
2025-07-24 03:13:19 +0200

    expand_wildcards norm OK

--- src/token/expand_wildcards.c
+++ src/token/expand_wildcards.c
@@ -6,13 +6,14 @@
 /*   By: abouclie <abouclie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/19 01:01:46 by abouclie          #+#    #+#             */
-/*   Updated: 2025/07/23 03:05:37 by abouclie         ###   ########.fr       */
+/*   Updated: 2025/07/24 01:29:03 by abouclie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexing.h"
 
-static void	process_entry(t_ctx *ctx, t_wildcards *wildcards, t_token **tokens);
+static void	build_and_process_path(t_ctx *ctx, t_wildcards *w,
+				char *slash, t_token **tokens);
 
 static char	*next_slash(const char *s)
 {
@@ -24,43 +25,58 @@ static char	*next_slash(const char *s)
 	return ((char *)(s + i));
 }
 
-static void	explore_wildcards(t_ctx *ctx, char *pattern, char *dir, t_token **tokens)
+static int	should_skip_entry(t_ctx *ctx, t_wildcards *w, char *part)
 {
-	t_wildcards	wildcards;
+	char		*slash;
 
-	wildcards.d = opendir(dir);
-	wildcards.pattern = pattern;
-	wildcards.dir = dir;
-	if (!wildcards.d)
-		return ;
-	while ((wildcards.e = readdir(wildcards.d)) != NULL)
-		process_entry(ctx, &wildcards, tokens);
-	closedir(wildcards.d);
+	if (!ft_strcmp(w->e->d_name, ".") || !ft_strcmp(w->e->d_name, ".."))
+		return (1);
+	slash = next_slash(w->pattern);
+	ft_strncpy(part, w->pattern, slash - w->pattern);
+	part[slash - w->pattern] = '\0';
+	if (w->e->d_name[0] == '.' && part[0] != '.')
+		return (1);
+	if (!wildcards_match(ctx, part, w->e->d_name, 0))
+		return (1);
+	return (0);
 }
 
-static void	process_entry(t_ctx *ctx, t_wildcards *wildcards, t_token **tokens)
+void	explore_wildcards(t_ctx *ctx, char *pattern, char *dir,
+			t_token **tokens)
 {
-	char		path[1024];
+	t_wildcards	wild;
 	char		part[256];
 	char		*slash;
+
+	wild.pattern = pattern;
+	wild.dir = dir;
+	wild.d = opendir(dir);
+	if (!wild.d)
+		return ;
+	wild.e = readdir(wild.d);
+	while (wild.e != NULL)
+	{
+		slash = next_slash(pattern);
+		if (!should_skip_entry(ctx, &wild, part))
+			build_and_process_path(ctx, &wild, slash, tokens);
+		wild.e = readdir(wild.d);
+	}
+	closedir(wild.d);
+}
+
+static void	build_and_process_path(t_ctx *ctx, t_wildcards *w, char *slash,
+		t_token **tokens)
+{
+	char		path[1024];
 	struct stat	st;
-	
-	if (!ft_strcmp(wildcards->e->d_name, ".") || !ft_strcmp(wildcards->e->d_name, ".."))
-		return ;
-	slash = next_slash(wildcards->pattern);
-	ft_strncpy(part, wildcards->pattern, slash - wildcards->pattern);
-	part[slash - wildcards->pattern] = '\0';
-	if (wildcards->e->d_name[0] == '.' && part[0] != '.')
-		return ;
-	if (!wildcards_match(ctx, part, wildcards->e->d_name, 0))
-		return ;
-	if (ft_strcmp(wildcards->dir, ".") == 0)
-		ft_strcpy(path, wildcards->e->d_name);
+
+	if (ft_strcmp(w->dir, ".") == 0)
+		ft_strcpy(path, w->e->d_name);
 	else
 	{
-		ft_strcpy(path, wildcards->dir);
+		ft_strcpy(path, w->dir);
 		ft_strcat(path, "/");
-		ft_strcat(path, wildcards->e->d_name);
+		ft_strcat(path, w->e->d_name);
 	}
 	if (*slash == '/')
 	{
@@ -69,7 +85,6 @@ static void	process_entry(t_ctx *ctx, t_wildcards *wildcards, t_token **tokens)
 	}
 	else if (*slash == '\0')
 		add_token(tokens, create_token(ctx, path, TOKEN_WORD));
-
 }
 
 t_token	*expand_wildcards(t_ctx *ctx, const char *pattern)
